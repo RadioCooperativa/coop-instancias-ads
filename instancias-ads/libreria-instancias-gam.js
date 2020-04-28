5c8a18ff8d4c3e7efff3e8db16bbd3554fc3a729
Rodrigo Vargas Sanhueza <rvargas@cooperativa.cl>
2020-04-27 23:26:53 -0400

    nueva implementacion

--- instancias-ads/libreria-instancias-gam.js
+++ instancias-ads/libreria-instancias-gam.js
@@ -1,6 +1,3 @@
-// LIBRERIA INSTANCIA GAM
-// window.googletag = window.googletag || {cmd: []};
-
 let ittFooter     = [1,1];
 let bannerTop     = [[728, 90], [990, 50], [728, 50], [728, 60], [990, 90], [990, 60], [728, 40], [990, 40], [1100, 90], [1300, 90]];
 let bannerTower   = [[120, 600],[160, 600]];
@@ -12,82 +9,52 @@ let MbannerTop     = [[300, 50], [300, 60], [300, 70], [300, 80], [300, 90], [30
 let MbannerRob     = [[300, 40], [300, 50], [300, 60], [300, 70], [300, 80], [300, 90], [300, 100], [300, 150], [300, 250], [300, 400], [300, 450], [300, 600], [320, 40], [320, 50], [320, 60], [320, 70], [320, 80], [320, 90], [320, 100], [320, 150], [320, 250]];
 
 let slotDimensions;
+let slot_ads = [];
 let arraySlotBlocks;
-
 
 const getCleanedString = function(cadena){
 
-     let specialChars = ["!@#$^&%*()+=-[]\/{}|:<>?,."];
-         specialChars.push('&amp;');
- 
-     for (let i = 0; i < specialChars.length; i++) {
-         cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
-     }   
- 
-     cadena = cadena.toLowerCase();
- 
-     cadena = cadena.replace(/ /g,"_");
- 
-     cadena = cadena.replace(/á/gi,"a");
-     cadena = cadena.replace(/é/gi,"e");
-     cadena = cadena.replace(/í/gi,"i");
-     cadena = cadena.replace(/ó/gi,"o");
-     cadena = cadena.replace(/ú/gi,"u");
-     //  cadena = cadena.replace(/ñ/gi,"n");
-     return cadena;
+    let specialChars = ["!@#$^&%*()+=-[]\/{}|:<>?,."];
+        specialChars.push('&amp;');
 
- }
-
-let dfp_sec         = getCleanedString(dfp_sec_);
-let coop_dfp_tipo       = getCleanedString(coop_tipo_);
-// let coop_dfp_region = getCleanedString(coop_dfp_region_); 
-
- //PARA ARTICULOS
-    
-    console.log("LIBRERIA INSTANCIA GAM dfp_sec: ",dfp_sec);
-    console.log("LIBRERIA INSTANCIA GAM coop_dfp_tipo: ",coop_dfp_tipo);
-    // console.log("LIBRERIA INSTANCIA GAM coop_dfp_region: ",coop_dfp_region);
-if(coop_dfp_tipo === 'articulo'){
-
-    console.log("LIBRERIA INSTANCIA GAM soy: "+coop_dfp_tipo);
-
-    let coop_dfp_seccion_1        = getCleanedString("<!--# echo var="NOMSECCION1" default="" -->")     || null;
-    let coop_dfp_seccion_2        = getCleanedString("<!--# echo var="NOMSECCION2" default="" -->")     || null;
-    let coop_dfp_seccion_3        = getCleanedString("<!--# echo var="NOMSECCION3" default="" -->")     || null;
-
-    let coop_dfp_tema_1           = getCleanedString("<!--# echo var="NOMTEMA1" default="" -->")        || null;
-    let coop_dfp_subtema_1        = getCleanedString("<!--# echo var="NOMSUBTEMA1" default="" -->")     || null;
-
-    let coop_dfp_tema_2           = getCleanedString("<!--# echo var="NOMTEMA2" default="" -->")        || null;
-    let coop_dfp_subtema_2        = getCleanedString("<!--# echo var="NOMSUBTEMA2" default="" -->")     || null;
-
-    let coop_dfp_tema_3           = getCleanedString("<!--# echo var="NOMTEMA3" default="" -->")        || null;
-    let coop_dfp_subtema_3        = getCleanedString("<!--# echo var="NOMSUBTEMA3" default="" -->")     || null;
-    let coop_dfp_ts               = "<!--# echo var="TS" default="" -->";
-
-    let arraySeccion = [];
-    arraySeccion.push(coop_dfp_seccion_1);
-    arraySeccion.push(coop_dfp_seccion_2);
-    arraySeccion.push(coop_dfp_seccion_3);
-
-    let arrayTem = [];
-    arrayTem.push(coop_dfp_tema_1);
-    arrayTem.push(coop_dfp_tema_2);
-    arrayTem.push(coop_dfp_tema_3);
-
-    let arrayStem = [];
-    arrayStem.push(coop_dfp_subtema_1);
-    arrayStem.push(coop_dfp_subtema_2);
-    arrayStem.push(coop_dfp_subtema_3);
+        if(cadena){
+           for (let i = 0; i < specialChars.length; i++) {
+               cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+           }   
+       
+           cadena = cadena.toLowerCase();
+       
+           cadena = cadena.replace(/ /g,"_");
+       
+           cadena = cadena.replace(/á/gi,"a");
+           cadena = cadena.replace(/é/gi,"e");
+           cadena = cadena.replace(/í/gi,"i");
+           cadena = cadena.replace(/ó/gi,"o");
+           cadena = cadena.replace(/ú/gi,"u");
+           //  cadena = cadena.replace(/ñ/gi,"n");
+           return cadena;
+        }
+        return null;
 }
-    
-  
-   
-let slot_ads = [];
 
-// console.log("coop_dfp_tipo: ",coop_dfp_tipo);
-// console.log("arrayTem: ",arrayTem);
-// console.log("dfp_sec: ",dfp_sec);
+    let coop_dfp_tipo       = getCleanedString(coop_tipo_);
+
+        arraySeccion.push(getCleanedString(coop_dfp_seccion_1));
+        arraySeccion.push(getCleanedString(coop_dfp_seccion_2));
+        arraySeccion.push(getCleanedString(coop_dfp_seccion_3));
+
+        arrayTem.push(getCleanedString(coop_dfp_tema_1));
+        arrayTem.push(getCleanedString(coop_dfp_tema_2));
+        arrayTem.push(getCleanedString(coop_dfp_tema_3));
+
+        arrayStem.push(getCleanedString(coop_dfp_subtema_1));
+        arrayStem.push(getCleanedString(coop_dfp_subtema_2));
+        arrayStem.push(getCleanedString(coop_dfp_subtema_3));
+
+        console.info("coop_dfp_tipo: "+coop_dfp_tipo);
+        console.info("arraySeccion: ",[arraySeccion.toString()]);
+        console.info("arrayTem: ",[arrayTem.toString()]);
+        console.info("arrayStem: ",[arrayStem.toString()]);
 
 
 (function($){
@@ -137,9 +104,6 @@ let slot_ads = [];
 
 const cargarPublicidad = function(){
 
-    console.log("LIBRERIA INSTANCIA GAM cargarPublicidad");
-
-
  if( !$(".coop_blockSlot")[0] ) return false;
      googletag.cmd.push(function() {
 
@@ -153,12 +117,6 @@ const cargarPublicidad = function(){
              let id         = $(this).data("adunit");
              let slot       = $(this).data("slot");
              let dimensions = $(this).data("dimensions");
-
-             console.log("cargarPublicidad id: ", id);
-             console.log("cargarPublicidad slot: ", slot);
-             console.log("cargarPublicidad dimensions: ", dimensions);
-
-
 
              switch(dimensions)            
             {
@@ -197,16 +155,13 @@ const cargarPublicidad = function(){
 
              slot_ads[i] = googletag.defineSlot(slot, slotDimensions, id).addService(googletag.pubads().setTargeting('coop_bloque', nomBloque));
 
-             console.log("cargarPublicidad coop_dfp_tipo: ",coop_dfp_tipo);
-            //  console.log("coop_tema_1: ",[arrayTem.toString()]);
-            //  console.log("coop_seccion_1: ",[arraySec.toString()]);
-
              googletag.pubads().enableSingleRequest();
              googletag.pubads().setCentering(true);
              googletag.pubads().collapseEmptyDivs(true,true);
              googletag.pubads().setTargeting('coop_dfp_tipo', coop_dfp_tipo);
-            //  googletag.pubads().setTargeting('coop_tema_1', [arrayTem.toString()]);
-            //  googletag.pubads().setTargeting('coop_seccion_1', dfp_sec);
+             googletag.pubads().setTargeting('coop_seccion_1', [arraySeccion.toString()]);
+             googletag.pubads().setTargeting('coop_tema_1', [arrayTem.toString()]);
+             googletag.pubads().setTargeting('coop_subtema_1', [arrayStem.toString()]);
 
              googletag.enableServices();
              googletag.display(id);
@@ -215,6 +170,11 @@ const cargarPublicidad = function(){
                  console.info("slot renderizados: ",event.slot.getSlotElementId());
              });
      });
+        for(let i =0; i<slot_ads.length; i++){
+            if (slot_ads[i].getSlotElementId() === 'coop_d_120x600_01_1'){
+                arraySlotBlocks = slot_ads[i];
+            }
+        }
  });
 }
 
@@ -222,7 +182,6 @@ const cargarPublicidad = function(){
 let process_scroll_focus = false;
 const cargarPublicidadFocus = function(){ 
     console.log("LIBRERIA INSTANCIA GAM cargarPublicidadFocus");
-
 
  if( !$(".coop_blockSlot")[0] ) return false;
  
@@ -241,10 +200,6 @@ const cargarPublicidadFocus = function(){
              let id         = $item.data("adunit");
              let slot       = $item.data("slot");
              let dimensions = $item.data("dimensions");
-
-             console.log("cargarPublicidadFocus id: ", id);
-             console.log("cargarPublicidadFocus slot: ", slot);
-             console.log("cargarPublicidadFocus dimensions: ", dimensions);
 
              if(dimensions === 4){
                 $item.css({                    
@@ -287,33 +242,27 @@ const cargarPublicidadFocus = function(){
                 }
 
                 const slotDiv = document.createElement('div');
-                 slotDiv.id  = id;
+                slotDiv.id  = id;
                 
-                 let nomBloqueFocus = slot.slice(9);
-                 console.log("cargarPublicidadFocus nomBloque: ",nomBloqueFocus);
-                //  data-slot="/1020719/coop_d_990x90_03"
-                 $item.html(slotDiv);
+                let nomBloqueFocus = slot.slice(9);
+                $item.html(slotDiv);
 
-                 slot_ads = googletag.defineSlot(slot, slotDimensions, id).addService(googletag.pubads().setTargeting('coop_bloque', nomBloqueFocus));
+                slot_ads = googletag.defineSlot(slot, slotDimensions, id).addService(googletag.pubads().setTargeting('coop_bloque', nomBloqueFocus));
 
-                 googletag.pubads().enableSingleRequest();
-                 googletag.pubads().setCentering(true);
-                 googletag.pubads().collapseEmptyDivs(true,true);
+                googletag.pubads().enableSingleRequest();
+                googletag.pubads().setCentering(true);
+                googletag.pubads().collapseEmptyDivs(true,true);
+                googletag.pubads().setTargeting('coop_dfp_tipo', coop_dfp_tipo);
+                googletag.pubads().setTargeting('coop_seccion_1', [arraySeccion.toString()]);
+                googletag.pubads().setTargeting('coop_tema_1', [arrayTem.toString()]);
+                googletag.pubads().setTargeting('coop_subtema_1', [arrayStem.toString()]);
 
-                // console.log("coop_dfp_tipo only-focus: ",coop_dfp_tipo);
-                // console.log("coop_tema_1 only-focus: ",[arrayTem.toString()]);
-                // console.log("coop_seccion_1 only-focus: ",[arraySec.toString()]);
-                 
-                //  googletag.pubads().setTargeting('coop_dfp_tipo', coop_dfp_tipo);
-                //  googletag.pubads().setTargeting('coop_tema_1', [arrayTem.toString()]);
-                //  googletag.pubads().setTargeting('coop_seccion_1', dfp_sec);
+                googletag.enableServices();
+                googletag.display(id);
 
-                 googletag.enableServices();
-                 googletag.display(id);
-
-                 googletag.pubads().addEventListener('slotRenderEnded', function (event) {
-                     console.info("slot renderizados: ",event.slot.getSlotElementId())
-                 });
+                googletag.pubads().addEventListener('slotRenderEnded', function (event) {
+                    console.info("slot renderizados: ",event.slot.getSlotElementId())
+                });
 
              process_scroll_focus = false;
          }
